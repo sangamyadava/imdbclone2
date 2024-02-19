ce25758e428288bcc9676dcc15990746aeedb93f
sangamyadava <sangamgkp49@gmail.com>
2024-02-19 23:19:50 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,100 @@
+// Get references to HTML elements
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+// Function to load movies from the OMDB API based on the search term
+async function loadMovies(searchTerm) {
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=9efe2270`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    if (data.Response == "True") {
+        // If the API response is successful, display the list of movies
+        displayMovieList(data.Search);
+    }
+}
+
+// Initial function call when searching for movies
+function findMovies() {
+    let searchTerm = (movieSearchBox.value).trim();
+    if (searchTerm.length > 0) {
+        // Display search list if there is a search term
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        // Hide search list if there is no search term
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+// Display the list of movies in the search results
+function displayMovieList(movies) {
+    searchList.innerHTML = "";
+    for (let idx = 0; idx < movies.length; idx++) {
+        // Create HTML elements for each movie in the search results
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movies[idx].imdbID; // Setting movie id in data-id
+        movieListItem.classList.add('search-list-item');
+        let moviePoster = (movies[idx].Poster != "N/A") ? movies[idx].Poster : "image_not_found.png";
+
+        // Populate the movie list item HTML
+        movieListItem.innerHTML = `
+            <div class="search-item-thumbnail">
+                <img src="${moviePoster}">
+            </div>
+            <div class="search-item-info">
+                <h3>${movies[idx].Title}</h3>
+                <p>${movies[idx].Year}</p>
+            </div>
+        `;
+        searchList.appendChild(movieListItem);
+    }
+    // Attach click event listeners to each movie item for loading details
+    loadMovieDetails();
+}
+
+// Load movie details when a movie is clicked in the search list
+function loadMovieDetails() {
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            // Fetch details for the selected movie
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9efe2270`);
+            const movieDetails = await result.json();
+            // Display the details of the selected movie
+            displayMovieDetails(movieDetails);
+        });
+    });
+}
+
+// Display detailed information about a selected movie
+function displayMovieDetails(details) {
+    resultGrid.innerHTML = `
+        <div class="movie-poster">
+            <img src="${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt="movie poster">
+        </div>
+        <div class="movie-info">
+            <h3 class="movie-title">${details.Title}</h3>
+            <ul class="movie-misc-info">
+                <li class="year">Year: ${details.Year}</li>
+                <li class="rated">Ratings: ${details.Rated}</li>
+                <li class="released">Released: ${details.Released}</li>
+            </ul>
+            <p class="genre"><b>Genre:</b> ${details.Genre}</p>
+            <p class="writer"><b>Writer:</b> ${details.Writer}</p>
+            <p class="actors"><b>Actors: </b>${details.Actors}</p>
+            <p class="plot"><b>Plot:</b> ${details.Plot}</p>
+            <p class="language"><b>Language:</b> ${details.Language}</p>
+            <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
+        </div>
+    `;
+}
+
+// Hide search list when clicking outside the search box
+window.addEventListener('click', (event) => {
+    if (event.target.className != "form-control") {
+        searchList.classList.add('hide-search-list');
+    }
+});
