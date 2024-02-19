03a3d6d2c9baee41165c34b38240ef09d13d01ee
sangamyadava <sangamgkp49@gmail.com>
2024-02-19 23:36:31 +0530

    http to https

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function loadMovieDetails() {
             // Fetch details for the selected movie
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9efe2270`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9efe2270`);
             const movieDetails = await result.json();
             // Display the details of the selected movie
             displayMovieDetails(movieDetails);
